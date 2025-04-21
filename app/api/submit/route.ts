dbad0509fca499ce7f990df5116a26470bc5012a
Kz <aikikizuki@gmail.com>
2025-04-21 10:59:13 +0900

    AC!!!!

--- app/api/submit/route.ts
+++ app/api/submit/route.ts
@@ -1,16 +1,18 @@
 import { NextRequest, NextResponse } from "next/server"
+import { createClient } from "@/utils/supabase/server"; 
 
 export async function POST(request: NextRequest) {
     const { name, participation } = await request.json()
 
     const supabase = await createClient()
 
-    const { data, error } = await supabase.from('submissions').insert({
+    const { data, error } = await supabase.from('forms').insert({
         name,
         participation
     })
 
     if (error) {
+        console.error("Supabase insert error:", error); // エラーログを追加
         return NextResponse.json({ error: error.message }, { status: 500 })
     }
 
